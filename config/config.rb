752ece3698df78f595cfdd59a7b72dd9a9c0c247
Jake Dahn <admin@jakedahn.com>
2010-01-31 04:58:52 -0600

    readding s3 info

--- config/config.rb
+++ config/config.rb
@@ -2,4 +2,8 @@ dbconfig = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection dbconfig["production"]
 
 # Replace this section with your s3 info
+AWS::S3::Base.establish_connection!(
+  :access_key_id     => '15JQ4C5YE4T94J0TC582',
+  :secret_access_key => 'CwBZjX4xWW/CZL3pigonVMXJ/boE+lPneggijjb9'
+)
 
